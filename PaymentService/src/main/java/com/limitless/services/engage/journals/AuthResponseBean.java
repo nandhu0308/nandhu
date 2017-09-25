8ad0baf4c33a160b9f6454c0d441442cfb2c5c0e
Anandh <Anandh@limitlesscircle.com>
2017-09-25 16:06:02 +0530

    commit

---
+++ PaymentService/src/main/java/com/limitless/services/engage/journals/AuthResponseBean.java
@@ -0,0 +1,15 @@
+package com.limitless.services.engage.journals;
+
+public class AuthResponseBean {
+	private boolean result;
+
+	public boolean isResult() {
+		return result;
+	}
+
+	public void setResult(boolean result) {
+		this.result = result;
+	}
+	
+
+}
